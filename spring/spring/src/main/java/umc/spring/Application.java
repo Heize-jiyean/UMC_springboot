668401d73c2ecc5abcc48a6856ca49d69dfa23a8
이지연 <writemumu@naver.com>
2024-11-21 18:06:39 +0900

    변경

--- spring/spring/src/main/java/umc/spring/Application.java
+++ spring/spring/src/main/java/umc/spring/Application.java
@@ -2,14 +2,12 @@ package umc.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;  // 이 줄 추가
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication  // 이것만으로도 umc.spring 패키지 이하를 모두 스캔합니다
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
